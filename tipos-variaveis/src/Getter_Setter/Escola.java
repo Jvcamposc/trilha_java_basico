8cffffcb1df3f5f40a164532ae43e805f8849bef
Jvcamposc <jvcamposc@gmail.com>
2024-09-21 08:18:16 -0300

    get e set

---
+++ tipos-variaveis/src/Getter_Setter/Escola.java
@@ -0,0 +1,11 @@
+package Getter_Setter;
+import java.util.Scanner;
+public class Escola {
+    public static void main(String[] args) {
+        Aluno felipe = new Aluno();
+        felipe.setNome("Felipe Junior");
+        felipe.setIdade(8);
+
+        System.out.println("O aluno " + felipe.nome + "tem" + felipe.idade + "anos");
+    }
+}
